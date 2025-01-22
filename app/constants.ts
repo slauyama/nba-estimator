a9f191d5c3ab57dfbef4e3b218257c25a9782e1f
Simon Lau-Yamauchi <7174954+slauyama@users.noreply.github.com>
2025-01-22 12:35:06 -0800

    Add commenting explaining limit for Conference Wins

--- app/constants.ts
+++ app/constants.ts
@@ -1,4 +1,11 @@
-export const TOTAL_GAMES = 1230;
 export const GAMES_IN_SEASON = 82;
+export const TEAMS = 30;
+export const TOTAL_GAMES = (GAMES_IN_SEASON * TEAMS) / 2; // 1230
+
+// Out of the 82 games, 30 are outside of conference and 52 are within the conference
+// Maximum wins for conference is winning all non conference games plus splitting conference games
+// (30 * 15) + (52 * 15 / 2)
 export const MAX_CONFERENCE_WINS = 840;
+// Minimum wins for conference is losing all non conference games plus splitting conference games
+// (0 * 15) + (52 * 15 / 2)
 export const MIN_CONFERENCE_WINS = 390;
